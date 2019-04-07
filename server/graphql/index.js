ee54df68fce52db4b68b2f248bf5e8df075ad7f8
Tuan Anh <ncq998@gmail.com>
2019-04-07 20:42:47 +0700

    Init project

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,8 +1,24 @@
+const merge = require('lodash/merge');
+
+const { ApolloServer } = require('apollo-server-express');
+import { makeExecutableSchema } from 'graphql-tools';
+
 
 
 
 const initializeApolloServer = (app) => {
+  const schema = makeExecutableSchema({
+    typeDefs: [],
+    resolvers: merge()
+  });
 
+  const server = new ApolloServer({
+    schema,
+    playground: {
+      endpoint: "/kappa"
+    }
+  });
+  server.applyMiddleware({ app })
 };
 
 module.exports = initializeApolloServer;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config({path: 'dev.env'});
 
 const app = require("./config/express");
 const initializeDb = require("./config/db");
 const initializeApolloServer = require("./graphql/index");
-
-
 
 initializeDb().then(() => {
     initializeApolloServer(app);

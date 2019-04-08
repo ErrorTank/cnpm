0402d58e17e00e57657f4a9103669a3cfb509ee4
Tuan Anh <ncq998@gmail.com>
2019-04-08 20:12:58 +0700

    Final init

--- client/react/loader.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {MainRoute} from "./routes/routes";
-
-ReactDOM.render(<MainRoute/>, document.getElementById("app"));
--- server/graphql/resolvers.js
+++
@@ -1,8 +0,0 @@
-const resolvers = {
-  Query: require('./resolvers/queries/Query'),
-  // Mutation: require('./resolvers/mutations/Mutation'),
-};
-
-module.exports = {
-  resolvers
-};

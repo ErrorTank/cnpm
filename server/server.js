b626fb002863cfc89c214b405cb75fc559ab83ce
Tuan Anh <ncq998@gmail.com>
2019-04-08 20:24:31 +0700

    move env to env file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: 'dev.env'});
+require('dotenv').config({path: './env/dev.env'});
 const express = require("express");
 const app = express();
 const initializeDb = require("./config/db");

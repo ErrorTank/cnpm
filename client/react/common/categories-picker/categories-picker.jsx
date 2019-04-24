25e86d747f9fe9b635b97d11cf6606a1d249bf73
Tuan Anh <ncq998@gmail.com>
2019-04-24 20:40:00 +0700

    add https

--- client/react/common/categories-picker/categories-picker.jsx
+++ client/react/common/categories-picker/categories-picker.jsx
@@ -8,7 +8,7 @@ export class CategoriesPicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentCategories: null
+      currentCategories: "Điện thoại - Máy tính bảng"
     };
   };
 

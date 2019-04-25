c032f0a9d5c08b264b83f1a6d20e3c27697560be
TuanAnh <ncq998@gmail.com>
2019-04-25 11:47:34 +0700

    Billing

--- server/server.js
+++ server/server.js
@@ -10,18 +10,18 @@ const fs = require("fs");
 const path = require("path");
 
 initializeDb().then(() => {
-  let environment = process.env.NODE_ENV;
-  let server = https.createServer(
-    {
-      key: fs.readFileSync(path.join(__dirname, `./ssl/${environment}/${process.env.SSL_KEY_PATH}`)),
-      cert: fs.readFileSync(path.join(__dirname, `./ssl/${environment}/${process.env.SSL_CRT_PATH}`))
-    },
-    app
-  );
+
   initializeApolloServer(app);
   configExpressServer(app);
   app.use('/', routerConfig());
-
+  let environment = process.env.NODE_ENV;
+  let server = https.createServer(
+      {
+        key: fs.readFileSync(path.join(__dirname, `./ssl/${environment}/${process.env.SSL_KEY_PATH}`)),
+        cert: fs.readFileSync(path.join(__dirname, `./ssl/${environment}/${process.env.SSL_CRT_PATH}`))
+      },
+      app
+  );
 
   server.listen(process.env.PORT, () => {
     console.log(`Server running on port: ${process.env.PORT}`);

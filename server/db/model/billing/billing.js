5470af3322197b552e59a282a2de5b3f5a6c8c06
TuanAnh <ncq998@gmail.com>
2019-04-25 11:39:38 +0700

    Billing

--- server/db/model/billing/billing.js
+++ server/db/model/billing/billing.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const ObjectId = mongoose.Schema.Types.ObjectId;
 // 0: Regular user, 1: Seller(include Regular)
 const billingSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    }
+    owner: {
+        type: ObjectId,
+        ref: "User"
+    },
+    products: {
+        type: [{
+            qty: Number,
+            details: {
+                type: ObjectId,
+                ref: "Product"
+            }
+        }]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: Boolean,
+        default: 0
+    },
+    total: Number
 });
 
 

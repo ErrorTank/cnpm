168316477a92709119307beea93bf39488b0f193
Tuan Anh <ncq998@gmail.com>
2019-05-09 17:56:39 +0700

    Finish zoom image

--- client/react/common/zoom-image/zoom-image.jsx
+++ client/react/common/zoom-image/zoom-image.jsx
@@ -6,33 +6,47 @@ export class ZoomImage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true,
-      posX: 200,
-      posY: 150
+      show: false,
+      left: 0,
+      top: 0
     };
   };
 
   zoomIn = (e) => {
-    // let overlayElem = ReactDOM.findDOMNode(this.imgOverlay);
-    // overlayElem.style.display = 'inline-block';
     if(this.state.show === false)
       this.setState({show: true});
     let imgElem = ReactDOM.findDOMNode(this.zoomingImg);
-    let posX = e.offsetX ? (e.offsetX) : e.pageX - imgElem.offsetLeft;
-    let posY = e.offsetY ? (e.offsetY) : e.pageY - imgElem.offsetTop;
-    this.setState({posX, posY});
+
+    let posX = e.offsetX ? (e.offsetX) : e.pageX - (imgElem.getBoundingClientRect().top + document.documentElement.scrollTop);
+    let posY = e.offsetY ? (e.offsetY) : e.pageY - imgElem.getBoundingClientRect().left;
+    let result = {};
+    if(posY <= 80.5){
+      result.top = 0;
+    }else if(imgElem.offsetHeight - posY <= 80.5){
+      result.top = imgElem.offsetHeight - 160;
+    }else{
+      result.top = posY - 80;
+    }
+    if(posX <= 80.5){
+      result.left = 0;
+    }else if(imgElem.offsetWidth - posX <= 80.5){
+      result.left = imgElem.offsetWidth - 160;
+    }else{
+      result.left = posX - 80;
+    }
+    this.setState({...result});
   };
 
+
+
+
   zoomOut = (e) => {
-    // let overlayElem = ReactDOM.findDOMNode(this.imgOverlay);
-    // overlayElem.style.display = 'none';
     this.setState({show: false});
   };
 
   render() {
     let {className, src} = this.props;
-    let {show, posX, posY} = this.state;
-    console.log((-posX * 2) + "px" + (-posY * 2) + "px")
+    let {show, left, top} = this.state;
     return (
       <div className={classnames("zoom-image-container", className)}>
         <div className="img-container">
@@ -41,22 +55,36 @@ export class ZoomImage extends React.Component {
                onMouseOut={this.zoomOut}
                ref={img => this.zoomingImg = img}
           />
-          {/*<div className="">*/}
-          {/**/}
-          {/*</div>*/}
-        </div>
-        <div className="img-zooming-overlay"
-          // ref={overlay => this.imgOverlay = overlay}
-             style={{
-               display: show ? "inline-block" : "none",
-               backgroundImage: `url(${src})`,
-               backgroundPosition: (-posX * 2) + "px " + (-posY * 2) + "px"
-             }}
-        >
+          {show && (
+            <div className="zoom-len"
+                 style={{
+                   left,
+                   top
+                 }}
+            >
+
+            </div>
+          )
+
+          }
 
         </div>
+        {show && (
+          <div className="img-zooming-overlay"
+               style={{
+                 display: "inline-block",
+                 backgroundImage: `url(${src})`,
+                 backgroundPosition: (-left * 3) + "px " + (-top * 3) + "px"
+               }}
+          >
+
+          </div>
+        )
+
+        }
+
 
       </div>
     );
   }
-}``
+}

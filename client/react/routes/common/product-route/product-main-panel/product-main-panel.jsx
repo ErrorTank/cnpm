ea472123ecef22b4fc95f918a7ea490ff2c72885
TuanAnh <ncq998@gmail.com>
2019-05-13 16:04:19 +0700

    fix desctibe fields

--- client/react/routes/common/product-route/product-main-panel/product-main-panel.jsx
+++ client/react/routes/common/product-route/product-main-panel/product-main-panel.jsx
@@ -3,6 +3,8 @@ import {ProductImagesDisplay} from "../../../../common/product-images-display/pr
 import {StarRating} from "../../../../common/star-rating/star-rating";
 import {customHistory} from "../../../routes";
 import {calcSalePrice, formatMoney} from "../../../../../common/products-utils";
+import {ProductMainInfo} from "./product-main-info";
+import {ProductHeaderInfo} from "./product-header-info";
 
 export class ProductMainPanel extends React.Component {
   constructor(props) {
@@ -40,42 +42,17 @@ const ProductLeftPanel = props => {
   )
 };
 
-const ProductRightPanel = ({commonInfo, optionInfo, onClickScrollToComment, onChangeOption}) => {
-  let {name, meanStar, commentCount, brand, regularDiscount} = commonInfo;
-  let {price, sold, description, total,} = optionInfo;
-  console.log(commonInfo)
+const ProductRightPanel = (props) => {
+
+  console.log(props)
   return (
     <div className="product-right-panel">
-      <div className="header-info">
-        <p className="product-name">{name}</p>
-        <StarRating
-          rating={meanStar}
-        />
-        <span className="scroll-to-comment" onClick={onClickScrollToComment}>(Xem {commentCount} đánh giá)</span>
-        <br/>
-        <span className="brand-name"
-              onClick={() => customHistory.push("/products?brand=" + brand._id)}>Thương hiệu: <span>{brand.name}</span></span>
-      </div>
-      <div className="main-info">
-        <div className="pricing-section">
-          <div className="main-price">Giá: <span
-            className="value">{formatMoney(calcSalePrice(Number(price), Number(regularDiscount)))} ₫</span></div>
-          {regularDiscount && (
-            <Fragment>
-              <div className="sale-sale">Tiết kiệm: <span
-                className="value">-{regularDiscount}%</span> ({formatMoney(Number(price) - calcSalePrice(Number(price), Number(regularDiscount)))} ₫)
-              </div>
-              <div className="sale-sale">Giá thị trường: {formatMoney(Number(price))} ₫
-              </div>
-            </Fragment>
-
-          )
-
-          }
-        </div>
-
-
-      </div>
+      <ProductHeaderInfo
+        {...props}
+      />
+      <ProductMainInfo
+        {...props}
+      />
     </div>
   )
 };

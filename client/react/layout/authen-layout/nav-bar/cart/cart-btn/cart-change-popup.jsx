e56104cedb3276cc28e4f1889cd539dc40fd0e6b
TuanAnh <ncq998@gmail.com>
2019-05-20 11:17:37 +0700

    dsadasdas

--- client/react/layout/authen-layout/nav-bar/cart/cart-btn/cart-change-popup.jsx
+++ client/react/layout/authen-layout/nav-bar/cart/cart-btn/cart-change-popup.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import {createNotificationPopup} from "../../../../../common/app-notification-popup/app-notification-popup";
+import {CSSTransition} from "react-transition-group";
 
 export const cartChangePopup = createNotificationPopup({
-  timeout: 4000
+  timeout: 20000
 });
 
 export const CartChangePopup = props => {
   return (
-    <div className="cart-change-popup">
-      {props.content}
-    </div>
+    <CSSTransition in={!!props.content} timeout={10000} classNames="cart-change-popup">
+      <div>
+        {props.content}
+      </div>
+    </CSSTransition>
   );
 
 };

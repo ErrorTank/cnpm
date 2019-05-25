940bb8368fc69303aadf04e9efa4d6f22671022f
Tuan Anh <ncq998@gmail.com>
2019-05-25 17:11:04 +0700

    do create cmt

--- client/react/common/star-rating/star-rating.jsx
+++ client/react/common/star-rating/star-rating.jsx
@@ -4,25 +4,32 @@ import classnames from "classnames";
 export class StarRating extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      rating: props.rating
+    };
   };
 
   render() {
-    let {className, rating = 0, } = this.props;
+    let {className, editable, onChange} = this.props;
+    let {rating = 0} = this.state;
 
     return (
-      <div className={classnames("stars-outer", className)}>
-        <span className="star"></span>
-        <span className="star"></span>
-        <span className="star"></span>
-        <span className="star"></span>
-        <span className="star"></span>
+      <div className={classnames("stars-outer", className, {editable})}
+           onMouseLeave={() => editable && this.setState({rating: this.props.rating || 0})}
+      >
+        {[1, 2, 3, 4, 5].map(each => (
+          <span className="star"
+                key={each}
+                onMouseEnter={() => editable && this.setState({rating: each})}
+                onClick={() => editable && onChange(each)}
+          />
+        ))}
         <div className="stars-inner" style={{width: (rating / 5) * 100 + "%"}}>
-          <span className="star"></span>
-          <span className="star"></span>
-          <span className="star"></span>
-          <span className="star"></span>
-          <span className="star"></span>
+          {[1, 2, 3, 4, 5].map(each => (
+            <span className="star"
+                  key={each}
+            />
+            ))}
         </div>
       </div>
     );
--- client/react/routes/common/product-route/comment-section/product-rating-section/rating-panel/rating-panel.jsx
+++ client/react/routes/common/product-route/comment-section/product-rating-section/rating-panel/rating-panel.jsx
@@ -1,15 +1,96 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {StarRating} from "../../../../../../common/star-rating/star-rating";
+import {KComponent} from "../../../../../../common/k-component";
+import {createSimpleForm} from "../../../../../../common/form-validator/form-validator";
+import {InputBase} from "../../../../../../common/base-input/base-input";
+import * as yup from "yup";
 
-export class RatingPanel extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-        };
-    };
-    render(){
-        return(
-            <div>
-            </div>
-        );
-    }
+export class RatingPanel extends KComponent {
+  constructor(props) {
+    super(props);
+    const schema = yup.object().shape({
+      star: yup.number().min(1, "Vui lòng chọn đánh giá của bạn về sản phẩm này."),
+      title: yup.string().max(100, "Tiêu đề không được quá 100 kí tự."),
+      content: yup.string().min(20, "Nội dung chứa ít nhất 20 ký tự").max(100, "Nội dung không được quá 500 kí tự."),
+      picture: yup.array()
+    });
+    this.form = createSimpleForm(schema, {
+      initData: {
+        star: 0,
+        title: "",
+        content: "",
+        picture: []
+      }
+    });
+    this.onUnmount(this.form.on("change", () => {
+      this.forceUpdate()
+    }));
+    this.form.validateData();
+  };
+
+  render() {
+    let canPost = this.form.getInvalidPaths().length === 0;
+    return (
+      <div className="rating-panel">
+        <div className="cmt-form m-form m-form--state">
+          <p className="rp-title">GỬI NHẬN XÉT CỦA BẠN</p>
+          <div className=" rp-form-gr">
+            <span className="label">1. Đánh giá của bạn về sản phẩm này:</span>
+            {this.form.enhanceComponent("star", ({onChange, value, error}) => (
+              <Fragment>
+                <StarRating
+                  className={"star-rating"}
+                  editable
+                  rating={value}
+                  onChange={onChange}
+                />
+                <p className="suggest">*Đánh giá là bắt buộc.</p>
+
+              </Fragment>
+
+
+            ), true)}
+
+          </div>
+          <div className="add-title rp-form-gr">
+            <span className="label">2. Tiêu đề của nhận xét:</span>
+            {this.form.enhanceComponent("title", ({error, onChange, onEnter,...others}) => (
+              <InputBase
+                className="rp-input pt-0"
+                error={error}
+                id={"title"}
+                type={"text"}
+                placeholder={"Nhập tiêu đề nhận xét (không bắt buộc)"}
+                onChange={e => {
+                  onChange(e);
+                }}
+                {...others}
+              />
+            ), true)}
+          </div>
+          <div className="add-content rp-form-gr">
+            <span className="label">3. Viết nhận xét của bạn vào bên dưới:</span>
+            {this.form.enhanceComponent("content", ({error, onChange, onEnter, ...others}) => (
+              <InputBase
+                className="rp-input pt-0"
+                error={error}
+                id={"content"}
+                type={"text"}
+                inputType={"textarea"}
+                placeholder={"Nhận xét của bạn về sản phẩm này"}
+                onChange={e => {
+                  onChange(e);
+                }}
+                {...others}
+              />
+            ), true)}
+          </div>
+          <div className="mt-5 text-right">
+            <button className="btn yellow-btn create-cmt" disabled={!canPost}>Gửi nhận xét</button>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }

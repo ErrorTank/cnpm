7df963ea188a012457e8b32b1564ffbd657df430
TuanAnh <ncq998@gmail.com>
2019-05-31 14:55:42 +0700

    lose img

---
+++ client/react/routes/guest-route/products-route/pagination-product-list/pagination-product-list.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class PaginationProductList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+        };
+    };
+    render(){
+        return(
+            <div className="pagination-product-list">
+            </div>
+        );
+    }
+}
--- client/react/routes/guest-route/products-route/products-route.jsx
+++ client/react/routes/guest-route/products-route/products-route.jsx
@@ -1,15 +1,48 @@
 import React from "react";
+import {AuthenLayout} from "../../../layout/authen-layout/authen-layout";
+import CheckoutCart from "../../common/cart-route/cart-route";
+import {PageTitle} from "../../../common/page-title/page-title";
+import {parseQueryString} from "../../../../string-utils";
+import {ProductFilterBar} from "./product-filter-bar/product-filter-bar";
+import {PaginationProductList} from "./pagination-product-list/pagination-product-list";
+import {MainProductFilter} from "./main-product-filter/main-product-filter";
 
-export class ProductsRoute extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-        };
+export class ProductsRoute extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainFilter: {
+        keyword: "",
+        mostSale: false,
+        descPrice: false,
+        ascPrice: false,
+        mostDiscount: true
+      },
+      productFilter: null,
     };
-    render(){
-        return(
-            <div>
+    const queryObj = parseQueryString(props.location.search);
+  };
+
+  render() {
+    return (
+      <PageTitle
+        title={""}
+      >
+        <AuthenLayout
+          showCategories={true}
+        >
+
+          <div className="container content-container products-route">
+            <div className="left-panel">
+              <ProductFilterBar/>
             </div>
-        );
-    }
+            <div className="right-panel">
+              <MainProductFilter/>
+              <PaginationProductList/>
+            </div>
+          </div>
+        </AuthenLayout>
+      </PageTitle>
+    );
+  }
 }

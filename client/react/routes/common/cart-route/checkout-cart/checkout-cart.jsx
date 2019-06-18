69c5654b07eebc470eb19da83dc695e250c0d65f
Main <hohohovip1@gmail.com>
2019-06-18 18:06:41 +0700

    add more to delete

--- client/react/routes/common/cart-route/checkout-cart/checkout-cart.jsx
+++ client/react/routes/common/cart-route/checkout-cart/checkout-cart.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import {createUserCartCacheFunction} from "../../../../../common/cache/cart-cache";
-import {userCart, userInfo} from "../../../../../common/states/common";
-import {KComponent} from '../../../../common/k-component';
-import {client} from "../../../../../graphql";
-import {getCartItemByIdList, addToCart} from "../../../../../graphql/queries/user";
-import {QuantityController} from "../../../../common/quantity-controller/quantity-controller";
-import {customHistory} from "../../../routes";
-import {formatMoney} from "../../../../../common/products-utils";
+import { createUserCartCacheFunction } from "../../../../../common/cache/cart-cache";
+import { userCart, userInfo } from "../../../../../common/states/common";
+import { KComponent } from '../../../../common/k-component';
+import { client } from "../../../../../graphql";
+import { getCartItemByIdList, addToCart,removeFromCart } from "../../../../../graphql/queries/user";
+import { QuantityController } from "../../../../common/quantity-controller/quantity-controller";
+import { customHistory } from "../../../routes";
+import { formatMoney } from "../../../../../common/products-utils";
 import Alert from "../../../../common/alert/alert";
-import {cartErrors} from "./cart-errors";
-import {VoucherInput} from "./voucher-input";
-import {Badge} from "../../../../common/badge/badge";
+import { cartErrors } from "./cart-errors";
+import { VoucherInput } from "./voucher-input";
+import { Badge } from "../../../../common/badge/badge";
+import { LoadingInline } from "../../../../common/loading-inline/loading-inline";
+
 
 class CheckoutCart extends KComponent {
   constructor(props) {
@@ -33,7 +35,7 @@ class CheckoutCart extends KComponent {
   };
 
   getOutStockError = () => {
-    let {cartItemList} = this.state;
+    let { cartItemList } = this.state;
     let outStockItems = cartItemList.filter(each => each.product.provider[0].options[0].sold >= each.product.provider[0].options[0].total);
     return outStockItems.map(each => {
       return {
@@ -47,7 +49,7 @@ class CheckoutCart extends KComponent {
   };
 
   getExceedError = () => {
-    let {cartItemList} = this.state;
+    let { cartItemList } = this.state;
     let exceedItems = cartItemList.filter(each => each.product.provider[0].options[0].total > each.product.provider[0].options[0].sold && each.quantity > (each.product.provider[0].options[0].total - each.product.provider[0].options[0].sold));
     return exceedItems.map(each => {
       return {
@@ -66,14 +68,14 @@ class CheckoutCart extends KComponent {
     let errors = [];
     errors = errors.concat(this.getOutStockError());
     errors = errors.concat(this.getExceedError());
-    this.setState({errors})
+    this.setState({ errors })
   };
 
 
   fetchItemList = () => {
     return new Promise((resolve, reject) => {
       let info = userInfo.getState();
-      let rawCart = info ? userCart.getState() : createUserCartCacheFunction("get")({async: false});
+      let rawCart = info ? userCart.getState() : createUserCartCacheFunction("get")({ async: false });
       let calculatePrice = 0;
       let cartCounting = 0;
       //console.log(rawCart);
@@ -83,7 +85,7 @@ class CheckoutCart extends KComponent {
           list: rawCart
         },
         fetchPolicy: "no-cache"
-      }).then(({data}) => {
+      }).then(({ data }) => {
         // console.log(this.state.cartItemList);
         let cartItemList = [...data.getCartItemByIdList];
         //console.log('keke', cartItemList);
@@ -96,7 +98,7 @@ class CheckoutCart extends KComponent {
           // console.log(founded.quantity);
           calculatePrice += ((e.product.provider[0].options[0].price / 100) * (100 - e.product.regularDiscount)) * founded.quantity;
           cartCounting += founded.quantity;
-          e = {...e, quantity: founded.quantity};
+          e = { ...e, quantity: founded.quantity };
           return e;
         });
         console.log(cartWithQuantity);
@@ -119,46 +121,52 @@ class CheckoutCart extends KComponent {
 
   };
 
-  handleDelete = (id) => {
+  handleDelete = (userID, optionID) => {
     let info = userInfo.getState();
     if (info) {
       return client.mutate({
-        mutation: addToCart,
+        mutation: removeFromCart,
         variables: {
-          pID: productID,
-          uID: info._id,
-          finalQty,
+          uID: userID,
           option: optionID
         }
-      }).then(({data}) => {
-
+      }).then(({ data }) => {
+        userCart.setState(data.removeFromCart.carts).then(() => {
+          let newCartList = this.state.cartItemList.filter(item => {
+            return item.product.provider[0].options[0]._id !== optionID;
+          })
+          console.log(newCartList);
+          this.setState({
+            cartItemList: newCartList
+          });
+        });
       });
     } else {
       createUserCartCacheFunction("set")({
-        product: productID,
-        quantity: finalQty,
+        uID: userID,
         option: optionID
       }).then(() => {
-
+        let newCartList = this.state.cartItemList.filter(item => {
+          return item.product.provider[0].options[0]._id !== optionID;
+        })
+        console.log(newCartList);
+        this.setState({
+          cartItemList: newCartList
+        });
       });
+      console.log('ok');
     }
-    let newCartList = this.state.cartItemList.filter(item => {
-      return item.product.provider[0].options[0]._id !== id;
-    })
-    this.setState({
-      cartItemList: item
-    })
   }
 
 
   handleQtyChange = (newQty, product, qty) => {
     // Set qty moi vao du lieu cua dung san pham dang thay doi
     // userCart.setState();
-    let {_id: productID} = product;
+    let { _id: productID } = product;
     let provider = product.provider;
     let finalQty = newQty - qty;
     let info = userInfo.getState();
-    let {_id: optionID} = provider[0].options[0];
+    let { _id: optionID } = provider[0].options[0];
     console.log(provider[0].options[0]);
     if (info) {
       return client.mutate({
@@ -169,7 +177,7 @@ class CheckoutCart extends KComponent {
           qty: finalQty,
           option: optionID
         }
-      }).then(({data}) => {
+      }).then(({ data }) => {
         userCart.setState(data.addToCart.carts).then(() => {
           let CartItemList = this.state.cartItemList;
           let newCartCount = this.state.cartCount;
@@ -183,8 +191,8 @@ class CheckoutCart extends KComponent {
             return p;
           })
           console.log(CartItemList)
-          this.setState({cartItemList: CartItemList, cartCount: newCartCount, totalPrice: newPrice}, () => {
-            this.setState({errors: this.state.errors.filter(each => each.type !== "qtyExceed").concat(this.getExceedError())})
+          this.setState({ cartItemList: CartItemList, cartCount: newCartCount, totalPrice: newPrice }, () => {
+            this.setState({ errors: this.state.errors.filter(each => each.type !== "qtyExceed").concat(this.getExceedError()) })
           });
         });
       });
@@ -207,8 +215,8 @@ class CheckoutCart extends KComponent {
         })
         console.log(CartItemList)
 
-        this.setState({cartItemList: CartItemList, cartCount: newCartCount, totalPrice: newPrice}, () => {
-          this.setState({errors: this.state.errors.filter(each => each.type !== "qtyExceed").concat(this.getExceedError())})
+        this.setState({ cartItemList: CartItemList, cartCount: newCartCount, totalPrice: newPrice }, () => {
+          this.setState({ errors: this.state.errors.filter(each => each.type !== "qtyExceed").concat(this.getExceedError()) })
         });
       });
     }
@@ -220,7 +228,7 @@ class CheckoutCart extends KComponent {
 
   handleApplyVoucher = (voucher) => {
 
-    let {cartItemList, errors, vouchers} = this.state;
+    let { cartItemList, errors, vouchers } = this.state;
     let allVouchers = cartItemList.reduce((result, each) => {
       return [...result, ...each.product.provider.filter(pro => pro.discountWithCode).map(pro => pro.discountWithCode)]
     }, []);
@@ -234,24 +242,24 @@ class CheckoutCart extends KComponent {
           }
         })
       })
-    }else{
-      if(!vouchers.find(each => each._id === voucherObj._id)){
-        this.setState({vouchers: vouchers.concat(voucherObj), errors: errors.filter(each => each.type !== "voucherError")})
+    } else {
+      if (!vouchers.find(each => each._id === voucherObj._id)) {
+        this.setState({ vouchers: vouchers.concat(voucherObj), errors: errors.filter(each => each.type !== "voucherError") })
       }
     }
   };
 
   render() {
     let info = userInfo.getState();
-    let {cartItemList, loading, totalPrice, cartCount} = this.state;
+    let { cartItemList, loading, totalPrice, cartCount } = this.state;
     //let rawCart = info ? userCart.getState() : createUserCartCacheFunction("get")({ async: false });
     let isDisabled = this.state.errors.find(each => each.type !== "voucherError");
     console.log(this.state.errors)
     let checkOut = cartCount !== 0 ? (
       <div className="checkout m-alert">
-        <div className="mt-5" style={{fontSize: "13px"}}>
+        <div className="mt-5" style={{ fontSize: "13px" }}>
           {this.state.errors.map((each, i) => {
-            let {type, data} = each;
+            let { type, data } = each;
             return (
               <Alert
                 className={"m-alert--outline"}
@@ -270,50 +278,50 @@ class CheckoutCart extends KComponent {
               <form id="shopping-cart">
                 {
                   cartItemList.map(item => {
-                      let {product, quantity} = item;
-                      let {_id, provider} = product;
-                      let {options, owner} = provider[0];
-                      let discountedPrice = (options[0].price / 100) * (100 - product.regularDiscount);
+                    let { product, quantity } = item;
+                    let { _id, provider } = product;
+                    let { options, owner } = provider[0];
+                    let discountedPrice = (options[0].price / 100) * (100 - product.regularDiscount);
 
-                      return (
-                        <div className="row shopping-cart-item" key={options[0]._id}>
-                          <div className="col-3 img-item-thumnail">
-                            <img src={options[0].picture[0]} alt="item-logo"/>
-                          </div>
-                          <div className="col-9 product-full-info pr-0">
-                            <div className="row">
-                              <div className="col-7 product-info">
-                                <p className="name"><span
-                                  onClick={() => customHistory.push("/product/" + product._id)}>{product.name} ({options[0].description})</span>
-                                </p>
-                                <p className="seller-by">Cung cấp bởi <span className="span-button"
-                                                                            onClick={() => customHistory.push(`/products?type=provider&provider=${owner._id}`)}>{owner.provider.name}</span>
-                                </p>      {/* seller-by who, did't found yet  */}
-                                <p className="action">
-                                  <span className="span-button" onClick={() => {
-                                    this.handleDelete(options[0]._id)
-                                  }}>Xóa</span>
-                                </p>
+                    return (
+                      <div className="row shopping-cart-item" key={options[0]._id}>
+                        <div className="col-3 img-item-thumnail">
+                          <img src={options[0].picture[0]} alt="item-logo" />
+                        </div>
+                        <div className="col-9 product-full-info pr-0">
+                          <div className="row">
+                            <div className="col-7 product-info">
+                              <p className="name"><span
+                                onClick={() => customHistory.push("/product/" + product._id)}>{product.name} ({options[0].description})</span>
+                              </p>
+                              <p className="seller-by">Cung cấp bởi <span className="span-button"
+                                onClick={() => customHistory.push(`/products?type=provider&provider=${owner._id}`)}>{owner.provider.name}</span>
+                              </p>      {/* seller-by who, did't found yet  */}
+                              <p className="action">
+                                <span className="span-button" onClick={() => {
+                                  this.handleDelete(owner._id, options[0]._id)
+                                }}>Xóa</span>
+                              </p>
+                            </div>
+                            <div className="col-3 box-price">
+                              <div className="price-1">{formatMoney(discountedPrice)}₫</div>
+                              <div className="price-2">
+                                <span className="full-price">{formatMoney(options[0].price)}₫</span>
+                                <span className="sale">| -{product.regularDiscount}%</span>
                               </div>
-                              <div className="col-3 box-price">
-                                <div className="price-1">{formatMoney(discountedPrice)}₫</div>
-                                <div className="price-2">
-                                  <span className="full-price">{formatMoney(options[0].price)}₫</span>
-                                  <span className="sale">| -{product.regularDiscount}%</span>
-                                </div>
-                              </div>
-                              <div className="col-2 quantity text-right">
-                                <QuantityController
-                                  value={quantity}
-                                  onChange={(qty) => this.handleQtyChange(qty, product, quantity)}
-                                  label={"Số lượng:"}
-                                />
-                              </div>
+                            </div>
+                            <div className="col-2 quantity text-right">
+                              <QuantityController
+                                value={quantity}
+                                onChange={(qty) => this.handleQtyChange(qty, product, quantity)}
+                                label={"Số lượng:"}
+                              />
                             </div>
                           </div>
                         </div>
-                      )
-                    }
+                      </div>
+                    )
+                  }
                   )
                 }
               </form>
@@ -334,7 +342,7 @@ class CheckoutCart extends KComponent {
                 </div>
               </div>
               <button className={`btn btn-danger checkout ${isDisabled ? `no-pointer-event disabled` : ""}`}
-                      type="button" disabled={isDisabled} onClick={() => this.handleSendBill()}>Tiến hành đặt hàng
+                type="button" disabled={isDisabled} onClick={() => this.handleSendBill()}>Tiến hành đặt hàng
               </button>
               <div className="add-voucher">
                 <div className="av-head">
@@ -348,8 +356,8 @@ class CheckoutCart extends KComponent {
                     {this.state.vouchers.map(each => {
                       return (
                         <Badge className={"voucher-badge"} content={(
-                          <span>{each.code} <i className="fas fa-times-circle" onClick={() => this.setState({vouchers: this.state.vouchers.filter(v => v._id !== each._id)})}></i></span>
-                        )} key={each._id}/>
+                          <span>{each.code} <i className="fas fa-times-circle" onClick={() => this.setState({ vouchers: this.state.vouchers.filter(v => v._id !== each._id) })}></i></span>
+                        )} key={each._id} />
                       )
                     })}
                   </div>
@@ -363,19 +371,19 @@ class CheckoutCart extends KComponent {
         </div>
       </div>
     ) : (
-      <div className="row">
-        <div className="col-xl-12">
-          <h5 className="cart-counting">Giỏ hàng <span>({cartCount} sản phấm)</span></h5>
-          <div className="container empty-cart ">
-            <span className="cart-icon"></span>
-            <p>Không có sản phẩm nào trong giỏ hàng của bạn!</p>
-            <button className={`btn yellow-btn `} type="button" onClick={() => customHistory.push("/")}>Tiếp tục mua
-              sắm
+        <div className="row">
+          <div className="col-xl-12">
+            <h5 className="cart-counting">Giỏ hàng <span>({cartCount} sản phấm)</span></h5>
+            <div className="container empty-cart ">
+              <span className="cart-icon"></span>
+              <p>Không có sản phẩm nào trong giỏ hàng của bạn!</p>
+              <button className={`btn yellow-btn `} type="button" onClick={() => customHistory.push("/")}>Tiếp tục mua
+                sắm
             </button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
     return (
       <div>
         {checkOut}

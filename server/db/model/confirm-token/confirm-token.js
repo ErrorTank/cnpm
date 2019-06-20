6bc8d5145673ac21f30dfe60ad800335532ad268
Tuan Anh <ncq998@gmail.com>
2019-06-20 11:58:23 +0700

    prepare for new shit

--- server/db/model/confirm-token/confirm-token.js
+++ server/db/model/confirm-token/confirm-token.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const tokenSchema =  new Schema({
   _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   token: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now, expires : 1800 }
+  createdAt: { type: Date, required: true, default: Date.now, expires : 1800 },
+  redirectUrl: String
 });
 // tokenSchema.index({ createdAt: 1 }, { expires : "2m" });
 

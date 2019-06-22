3e9d896a9b58f50971884cf251f13265c2299d4e
Tuan Anh <ncq998@gmail.com>
2019-06-22 10:00:25 +0700

    4231423424234

--- client/api/common/user-api.js
+++ client/api/common/user-api.js
@@ -1,7 +1,7 @@
 import {authenApi} from "../api";
 
 
-export const productApi = {
+export const userApi = {
   updateUserInfo({userID, change}){
     return authenApi.postMultipart(`/user/${userID}/update`, change, "picture")
   }

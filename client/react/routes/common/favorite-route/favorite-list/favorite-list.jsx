e4173bfef968038aff7c9ca99bab4e0d1e662820
Hoang <hohohovip1@gmail.com>
2019-06-22 15:46:47 +0700

    adfad

--- client/react/routes/common/favorite-route/favorite-list/favorite-list.jsx
+++ client/react/routes/common/favorite-route/favorite-list/favorite-list.jsx
@@ -2,15 +2,45 @@ import React from 'react'
 import { userFavorites, userInfo } from "../../../../../common/states/common";
 import { KComponent } from '../../../../common/k-component';
 import { formatMoney } from "../../../../../common/products-utils";
+import { appStoreController, clearAppStores } from "../../../../../common/system/system";
+import { client } from "../../../../../graphql";
+import { getFavItemsByIdList } from "../../../../../graphql/queries/user";
 
 class FavoriteList extends KComponent{
     constructor(props) {
     super(props);
-    // this.onUnmount(appStoreController.onChange(() => {
-    //   this.setState({ ...this.defaultState }, () => this.fetchItemList());
-    // }));
+    this.fetchItemList();
     // Call api only list not empty
   };
+
+  fetchItemList = () => {
+      return new Promise((resolve, reject) => {
+          let rawCart = userFavorites.getState();
+          client.query({
+              query: getFavItemsByIdList,
+              variables: {
+                  list: rawCart
+              },
+              fetchPolicy: "no-cache"
+          }).then(({ data }) => {
+              let favoriteItemList = [...data.getFavItemsByIdList];
+              console.log(favoriteItemList);
+              this.setState({
+                  favoriteItemList: favoriteItemList,
+                  loading: false,
+              }, () => {
+                  this.updateError();
+                  resolve();
+              });
+          }).catch(err => {
+              // console.log(err);
+              reject();
+          });
+
+      });
+
+  }
+
   render(){
       let rawCart = userFavorites.getState()
       console.log(rawCart);

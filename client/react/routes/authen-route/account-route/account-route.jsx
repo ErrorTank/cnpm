e095ba3ce25f4c6e798467fb98db7f02519828d0
Đào Tuấn Trung <termanteus@aos-iMac.local>
2019-06-22 15:21:35 +0700

    UpdateUserInfo without backend

--- client/react/routes/authen-route/account-route/account-route.jsx
+++ client/react/routes/authen-route/account-route/account-route.jsx
@@ -1,28 +1,22 @@
 import React, { Component } from "react";
-// import { PageTitle } from "../../common/page-title/page-title";
-// import { AuthenLayout } from "../../../layout/authen-layout/authen-layout";
-import {PageTitle} from "../../../common/page-title/page-title"
-import {AuthenLayout} from "../../../layout/authen-layout/authen-layout"
+import { PageTitle } from "../../../common/page-title/page-title";
+import { AuthenLayout } from "../../../layout/authen-layout/authen-layout";
+import AccountInfo from "./account-info/account-info";
 
-export default class AccountRoute extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-        };
-    };
-    render(){
-        return(
-            <PageTitle
-                title={"Tài khoản của tôi"}
-            >
-                <AuthenLayout
-                    showCategories={true}
-                >
-                    <div className="container content-container">
-                    </div>
-
-                </AuthenLayout>
-            </PageTitle>
-        );
-    }
+export default class AccountRoute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <PageTitle title={"Tài khoản của tôi"}>
+        <AuthenLayout showCategories={true}>
+          <div className="container content-container">
+            <AccountInfo />
+          </div>
+        </AuthenLayout>
+      </PageTitle>
+    );
+  }
 }

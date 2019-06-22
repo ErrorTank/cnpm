f0813f7dfe47cb8aa211da6c03f1340619615df2
Tuan Anh <ncq998@gmail.com>
2019-06-22 10:12:07 +0700

    31231312

--- client/graphql/utils/errors.js
+++ client/graphql/utils/errors.js
@@ -1,7 +1,7 @@
 const getErrorObject = err => {
   let errJson = JSON.parse(JSON.stringify(err));
-  return errJson ? graphQLErrors[0] : {};
-}
+  return errJson ? errJson.graphQLErrors[0] : {};
+};
 
 export {
   getErrorObject
